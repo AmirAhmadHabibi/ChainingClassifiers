f001005934891cccf97e18356a5330a9609fe20c
Amirahmadhabibi <amirahmad.habibi@gmail.com>
2019-02-18 14:30:45 -0700

    fixed the problem with PCA dimension reduction + minor refactorings

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import dimension_reducer
 from analyse import find_best_kernel_widths, do_analysis
 from predict import predict_with_all_kernel_widths, predict_all_models
 
+START = 1940
+THRESHOLD = 1950
+END = 2010
 # prepare the data and the word2vec
 model_saver.save_chi_w2vs()
 
@@ -15,7 +18,7 @@ model_saver.w2v_remove_non_superword()
 
 dimension_reducer.reduce_dimensions(method='PCA')
 dimension_reducer.reduce_dimensions(method='LDA')
-dimension_reducer.reduce_dimensions(method='LDA', threshold=1950)
+dimension_reducer.reduce_dimensions(method='LDA', threshold=THRESHOLD)
 
 # 4 different vector spaces
 paths = {'orig': 'chi-w2v-yby-all', 'PCA': 'chi-w2v-yby-pca-all', 's0.5LDA-fixed': 'chi-w2v-yby-s0.5lda-fixed-all',
@@ -60,11 +63,12 @@ kernel_widths = [i / 10.0 for i in range(1, 11)] + [float(i) for i in range(2, 1
 # for each type of vector space
 for v, p in paths.items():
     # for all kernel_widths try to predict and then analyse to find the best KW
-    predict_with_all_kernel_widths(path=p, w2v_version=v, kws=kernel_widths, models=kw_models, s=1940, t=1941, e=1950)
+    predict_with_all_kernel_widths(path=p, w2v_version=v, kws=kernel_widths, models=kw_models, s=START, t=START + 1,
+                                   e=THRESHOLD)
     find_best_kernel_widths(path=p, kws=kernel_widths, models=kw_models)
 
     # having the best kernel widths, predict with all models
-    predict_all_models(path=p, w2v_version=v, models=models, s=1940, t=1951, e=2010)
+    predict_all_models(path=p, w2v_version=v, models=models, s=START, t=THRESHOLD + 1, e=END)
 
     # analyse the predictions and get the precision values
     do_analysis(path=p, models=models)
